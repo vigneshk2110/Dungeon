df25355573e1bc5e966108bac824b92301826f75
VigneshK <104199131+vigneshk2110@users.noreply.github.com>
2022-10-06 22:03:09 +0530

    Add files via upload

---
+++ ManAndMonsterPath.java
@@ -0,0 +1,29 @@
+package dungeon;
+
+public class ManAndMonsterPath {
+
+	public static void main(String[] args) {
+		Dungeon d = new Dungeon(5, 4);
+		ObjectPosition man = new ObjectPosition(1, 3, "Man");
+		ObjectPosition gold = new ObjectPosition(3, 2, "Gold");
+		d = d.addObject(man);
+		d = d.addObject(gold);
+		ObjectPosition monster = new ObjectPosition(5, 1, "Monster");
+		d = d.addObject(monster);
+		d.print();
+		
+		
+		int manAndGoldDis = d.shortDistance(man,gold);
+//		System.out.println(manAndGoldDis + "--- Man & Gold");
+		int monsterAndGoldDis = d.shortDistance(monster,gold);
+//		System.out.println(monsterAndGoldDis + "--- Monster & Gold");
+		
+		if (manAndGoldDis<=monsterAndGoldDis) {
+			System.out.println(manAndGoldDis + " Units Distance");
+			d.printPath(man,gold);
+		} else {
+			System.out.println("No Possible Solution");
+		}
+	}
+
+}
